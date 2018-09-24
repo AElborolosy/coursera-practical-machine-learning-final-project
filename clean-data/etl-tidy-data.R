b55b1309918b9e3e5f8b4501f55febc829aa966a
Ahmed Elborolosy <aelborol@Excellus.com>
2018-09-24 15:23:34 -0400

    Testing the use of a classification decision tree via the rpart library

--- clean-data/etl-tidy-data.R
+++ clean-data/etl-tidy-data.R
@@ -1,9 +1,18 @@
 # Script that transform the training and testing data sets into a format
 # appropiate for machine learning algorithms.
 library(dplyr)
-
+library(rpart)
 
 training.data <-
   read.csv(file = "./data/pml-training.csv", row.names = 1) %>%
   as.tbl() %>%
   arrange(user_name, classe)
+
+# Hyperparameters still need to be optimized. See documentation.
+decision.tree <- rpart::rpart(formula = classe ~ .,
+                              data = training.data,
+                              model = "class")
+
+print(decision.tree)
+print(summary(decision.tree))
+rpart::printcp(decision.tree)	
